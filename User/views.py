a6a0017268ef6e7fea18a089e55305874aff7cb4
tusharGOEL1 <tushargoel_199@yahoo.com>
2017-01-28 12:46:00 +0530

    aavvs

--- User/views.py
+++ User/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from models import User
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 
 class IndexView(generic.ListView):
 	template_name = 'User/index.html'
