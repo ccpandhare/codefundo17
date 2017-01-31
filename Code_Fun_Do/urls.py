50885529dbc86d088edcee647b1c3a7fb76af04f
Megh-Thakkar <megh.1211@gmail.com>
2017-01-31 19:22:50 +0530

    Will work

--- Code_Fun_Do/urls.py
+++ Code_Fun_Do/urls.py
@@ -17,11 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('User.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
